68f8f6c842fb867e03be1ee69db864df546642b2
cedric-charlesia <ccharlesia@gmail.com>
2023-01-16 11:45:26 +0100

    feat: model update to get only working defibrilators in findAllDefibrilators function (commented code)

--- app/models/defibrilator.js
+++ app/models/defibrilator.js
@@ -11,8 +11,21 @@ class Defibrilator {
     try {
       const { rows } = await client.query('SELECT * FROM "defibrilators"');
 
+      // Get all defibrilators
+
       if (rows) return rows.map((row) => new Defibrilator(row));
       else return null;
+
+      // Get only working defibrilators (state = 0) :
+
+      /* let workingDefibrilators = [];
+      rows.forEach((row) => {
+        if (row.state === 0) {
+          workingDefibrilators.push(new Defibrilator(row));
+        }
+      });
+      return workingDefibrilators;
+      */
     } catch (error) {
       if (error.detail) {
         throw new Error(error.detail);
